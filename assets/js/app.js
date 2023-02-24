ca7a55c3086c48ad6f43b49731ba8ec12682fd9d
Doanhaiduy <haiduytbt2k3@gmail.com>
2023-02-25 01:40:02 +0700

    initial form val

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+function invalid(fullName, email, password, cf_password) {
+    const regex_name =
+        /^[A-ZÀÁẠẢÃÂẦẤẬẨẪĂẰẮẶẲẴÈÉẸẺẼÊỀẾỆỂỄÌÍỊỈĨÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠÙÚỤỦŨƯỪỨỰỬỮỲÝỴỶỸĐ][a-zàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđ]*(?:[ ][A-ZÀÁẠẢÃÂẦẤẬẨẪĂẰẮẶẲẴÈÉẸẺẼÊỀẾỆỂỄÌÍỊỈĨÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠÙÚỤỦŨƯỪỨỰỬỮỲÝỴỶỸĐ][a-zàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđ]*)*$/gm;
+    const regex_email =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regex_pass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (regex_name.test(fullName) && regex_email.test(email) && regex_pass.test(password) && password === cf_password) {
+        console.log("valid");
+        return true;
+    } else {
+        console.log("not valid");
+        return false;
+    }
+}
+
+function fail_login(btn) {
+    btn.classList.add("btn_login-invalid");
+    setTimeout(() => {
+        btn.classList.remove("btn_login-invalid");
+    }, 1000);
+}
+
+function fail_register(btn) {
+    btn.classList.add("btn_register-invalid");
+    setTimeout(() => {
+        btn.classList.remove("btn_register-invalid");
+    }, 1000);
+}
